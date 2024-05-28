5020a60f70f83275dc5440f4818be4cc7588cb05
bdoolaeghe <bruno.doolaeghe.ext@blablacar.com>
2024-05-28 10:48:04 +0200

    Tests red

---
+++ src/test/java/com/neosoft/interviewback/service/NeoSoFtServiceTest.java
@@ -0,0 +1,29 @@
+package com.neosoft.interviewback.service;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static java.lang.Integer.parseInt;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class NeoSoFtServiceTest {
+
+    NeoSoFtService neoSoFtService = new NeoSoFtService();
+
+    @ParameterizedTest
+    @CsvSource(value = {
+        "1,1",
+        "3,NeoNeo",
+        "5,SoSo",
+        "7,Ft",
+        "9,Neo",
+        "51,NeoSo",
+        "53,SoNeo",
+        "33,NeoNeoNeo",
+        "27,NeoFt",
+        "15,NeoSoSo"
+    })
+    void should_validate_test_harness(String input,String expectedOutput) {
+        assertThat(neoSoFtService.convertNumber(parseInt(input))).isEqualTo(expectedOutput);
+    }
+}

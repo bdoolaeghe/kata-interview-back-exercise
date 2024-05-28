f3915cf669032e341d034ada1a9ec87387f8c0fc
bdoolaeghe <bruno.doolaeghe.ext@blablacar.com>
2024-05-28 09:54:25 +0200

    Refactor divisors

--- src/main/java/com/neosoft/interviewback/service/NeoSoFtService.java
+++ src/main/java/com/neosoft/interviewback/service/NeoSoFtService.java
@@ -7,25 +7,31 @@ import java.util.stream.Stream;
 @Service
 public class NeoSoFtService {
 
+    public static final String NEO = "Neo";
+    public static final String SO = "So";
+    public static final String FT = "Ft";
+
     public String convertNumber(int inputNumber) {
         var result = new StringBuilder();
-        if (inputNumber % 3 == 0) {
-            result.append("Neo");
-        }
-        if (inputNumber % 5 == 0) {
-            result.append("So");
-        }
+
+        result.append(replaceWhenDivisible(inputNumber, 3, NEO));
+        result.append(replaceWhenDivisible(inputNumber, 5, SO));
+
         var content = String.valueOf(inputNumber).split("");
         Stream.of(content).forEach(c -> {
                     var mapping = switch (c) {
-                        case "3" -> "Neo";
-                        case "5" -> "So";
-                        case "7" -> "Ft";
+                        case "3" -> NEO;
+                        case "5" -> SO;
+                        case "7" -> FT;
                         default -> "";
                     };
                     result.append(mapping);
                 }
         );
         return result.isEmpty() ? String.valueOf(inputNumber) : result.toString();
+    }
+
+    private String replaceWhenDivisible(int inputNumber, int divisor, String replacement) {
+        return (inputNumber % divisor == 0) ? replacement : "";
     }
 }

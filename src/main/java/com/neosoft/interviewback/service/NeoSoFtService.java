75628e9e720d02f6cc8db3396260e08de78b70f2
bdoolaeghe <bruno.doolaeghe.ext@blablacar.com>
2024-05-28 09:45:39 +0200

    Tests green

--- src/main/java/com/neosoft/interviewback/service/NeoSoFtService.java
+++ src/main/java/com/neosoft/interviewback/service/NeoSoFtService.java
@@ -2,10 +2,30 @@ package com.neosoft.interviewback.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.stream.Stream;
+
 @Service
 public class NeoSoFtService {
 
     public String convertNumber(int inputNumber) {
-        throw new RuntimeException("implement me !");
+        var result = new StringBuilder();
+        if (inputNumber % 3 == 0) {
+            result.append("Neo");
+        }
+        if (inputNumber % 5 == 0) {
+            result.append("So");
+        }
+        var content = String.valueOf(inputNumber).split("");
+        Stream.of(content).forEach(c -> {
+                    var mapping = switch (c) {
+                        case "3" -> "Neo";
+                        case "5" -> "So";
+                        case "7" -> "Ft";
+                        default -> "";
+                    };
+                    result.append(mapping);
+                }
+        );
+        return result.isEmpty() ? String.valueOf(inputNumber) : result.toString();
     }
 }
